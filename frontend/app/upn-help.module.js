4207aab71585756a431e44a4895934ecaa7965eb
Michael BAILLY <mbailly@linagora.com>
2020-07-07 17:09:05 +0200

    [feat] Add UPN Home page

--- frontend/app/upn-help.module.js
+++ frontend/app/upn-help.module.js
@@ -7,5 +7,25 @@
     'esn.core',
     'op.dynamicDirective',
     'esn.module-registry'
-  ]);
+  ])
+  .constant('UPN_MODULE_METADATA', {
+      id: 'linagora.esn.upn-help',
+      title: 'UPN Home',
+      homePage: 'UPN',
+      icon: '/images/application.png',
+      disableable: false,
+      isDisplayedByDefault: false
+    })
+    .run(function(esnModuleRegistry, UPN_MODULE_METADATA) {
+      esnModuleRegistry.add(UPN_MODULE_METADATA);
+    })
+    .config(function($stateProvider) {
+      $stateProvider.state({
+        name: 'UPN',
+        url: '/home',
+        template: '<p>&nbsp;<br />&nbsp;</p><p>Nous vous prions de nous excuser pour la gêne occasionnée.<br />' +
+                  'Vous êtes invités à utiliser la messagerie en ligne sur <a href="https://webmail.parisnanterre.fr">https://webmail.parisnanterre.fr</a>.</p>'
+      });
+    });
+
 })(angular);
